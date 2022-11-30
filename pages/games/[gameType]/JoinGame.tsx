a3e64502e3b3a28fe900c5abb51adcb89b528a60
poludnev <poludnev.roman@gmail.com>
2022-11-30 12:26:02 +0300

    Added Join game page

--- pages/games/[gameType]/JoinGame.tsx
+++ pages/games/[gameType]/JoinGame.tsx
@@ -1,5 +1,42 @@
+import { Button } from 'components';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from 'styles/JoinGame.module.scss';
 const JoinGame = () => {
-  return <div className={styles.container}>GAME TYPE JOIN GAME</div>;
+  const [gameList, setGameList] = useState();
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const { value } = event.currentTarget.gameid;
+    //TODO: add gameId handler
+    console.log(router.query);
+    const { gameType } = router.query as { gameType: string };
+    router.push(`/games/${gameType}?gameid=${value}`);
+  };
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>{t('joinGame.title')}</h1>
+      <div className={styles.description}>{t('joinGame.description')}</div>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <div className={styles.inputgroup}>
+          <input
+            id="gameid"
+            className={styles.input}
+            placeholder="ttt-000"
+            autoComplete="off"
+            name="gameid"
+          ></input>
+          <label htmlFor="gameid" className={styles.label}>
+            {t('joinGame.gameid')}
+          </label>
+        </div>
+        <Button type="submit" value="submit" width="w200" color="black" />
+      </form>
+      {gameList && <div className={styles.description}>{t('joinGame.choseFromlist')}</div>}
+    </div>
+  );
 };
 export default JoinGame;
